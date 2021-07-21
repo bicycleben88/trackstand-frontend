5f9e49e1c4a6bda070ba6123abe30b6ce8748cdf
Ben Higginbotham <bicycleben88@gmail.com>
2021-07-20 21:11:43 -0400

    add user state and handle change for username

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -2,13 +2,22 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 export default function Signup() {
-  const [text, setText] = useState('');
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e, name) => {
+    const {text} = e.nativeEvent;
+    setUser({...user, [name]: text});
+  };
+
   return (
     <View style={styles.signupContainer}>
       <TextInput
         placeholder="username"
-        onChangeText={text => setText(text)}
-        defaultValue={text}
+        onChange={e => handleChange(e, 'name')}
+        defaultValue={user.username}
       />
     </View>
   );
