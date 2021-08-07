57e5504d030307b07806f59a489858a36468c432
Ben Higginbotham <bicycleben88@gmail.com>
2021-08-06 23:31:03 -0400

    add apollo client

--- App.tsx
+++ App.tsx
@@ -6,22 +6,27 @@
  * @flow strict-local
  */
 
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
 import React, {Node} from 'react';
-import {SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
-import Signup from './components/Signup';
-import Boilerplate from './components/Boilerplate';
+import {Button, SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
+
+const client = new ApolloClient({
+  uri: 'localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const App: () => Node = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView>
-        <View>
-          {/* <Signup /> */}
-          <Boilerplate />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ApolloProvider client={client}>
+      <Button
+        title="Log In"
+        onPress={() => console.log('TO DO: make this button do something')}
+      />
+      <Button
+        title="Sign Up"
+        onPress={() => console.log('TO DO: make this button do something')}
+      />
+    </ApolloProvider>
   );
 };
 
