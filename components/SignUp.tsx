a430700a9bcf577fde6ba244e2e5e76b337ccd77
Ben Higginbotham <bicycleben88@gmail.com>
2021-08-07 23:35:59 -0400

    add sign up component NOT hooked up to graphQL

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {Text, TextInput, View} from 'react-native';
 
 export default function SignUp() {
@@ -9,12 +10,40 @@ export default function SignUp() {
   });
 
   const handleChange = (inputType, e) => {
-    setUser({...user, [inputType]: e.nativeTarget.value});
+    setUser({...user, [inputType]: e.nativeEvent.text});
   };
 
   return (
     <View>
-      <TextInput value={user.name} onChange={e => handleChange('name', e)} />
+      <TextInput
+        style={styles.form}
+        value={user.name}
+        onChange={e => handleChange('name', e)}
+        placeholder="name"
+      />
+      <TextInput
+        style={styles.form}
+        value={user.email}
+        onChange={e => handleChange('email', e)}
+        placeholder="email"
+      />
+      <TextInput
+        style={styles.form}
+        value={user.password}
+        onChange={e => handleChange('password', e)}
+        placeholder="password"
+        secureTextEntry={true}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    borderColor: 'black',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
