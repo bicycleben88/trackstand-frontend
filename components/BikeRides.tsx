c22f5208b71f3df9918a1327bb148b7d9fb64024
Ben Higginbotham <bicycleben88@gmail.com>
2021-09-03 23:09:53 -0400

    query bike rides when user logs in

--- components/BikeRides.tsx
+++ components/BikeRides.tsx
@@ -1,45 +1,26 @@
-import {gql, useQuery} from '@apollo/client';
 import React from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {useUser} from '../lib/user';
 import {globalStyles} from '../styles/globalStyles';
 
-const {globalPaddingHorizontal, globalPaddingVertical, h3, h5} = globalStyles;
-
-const USER_BIKERIDES = gql`
-  query USER_BIKERIDES {
-    allBikeRides(where: {user: {id: "ckqxy1tbb0012fx01zu0m3z41"}}) {
-      date
-      miles
-      hours
-      minutes
-      user {
-        name
-      }
-    }
-  }
-`;
+const {globalPaddingHorizontal, globalPaddingVertical, h3, h5, headerColor} =
+  globalStyles;
 
 export default function BikeRides() {
-  const {data, loading, error} = useQuery(USER_BIKERIDES);
   const loggedInUser = useUser();
 
   if (!loggedInUser) {
     return <Text>Please Log in to view Bike Rides</Text>;
   }
 
-  const {allBikeRides} = data;
+  const {bikeRide} = loggedInUser;
 
   return (
     <View style={styled.container}>
-      {loading && <Text>Fetching Bike Rides...</Text>}
-      {error && <Text>Failed to Fetch Bike Rides...</Text>}
-      <Text style={styled.header}>
-        {allBikeRides[0].user.name}'s Bike Rides
-      </Text>
+      <Text style={styled.header}>{loggedInUser.name}'s Bike Rides</Text>
       <View>
         <FlatList
-          data={allBikeRides}
+          data={bikeRide}
           keyExtractor={({id}) => id}
           renderItem={({item}) => (
             <>
@@ -66,6 +47,7 @@ const styled = StyleSheet.create({
     textAlign: 'center',
   },
   date: {
+    backgroundColor: headerColor,
     fontSize: h5,
   },
 });
--- lib/user.ts
+++ lib/user.ts
@@ -7,10 +7,17 @@ const CURRENT_USER_QUERY = gql`
         id
         email
         name
+        bikeRide {
+          date
+          miles
+          hours
+          minutes
+        }
       }
     }
   }
 `;
+
 export function useUser() {
   const {data} = useQuery(CURRENT_USER_QUERY);
   return data?.authenticatedItem;
