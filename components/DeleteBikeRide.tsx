541a64ac27bffd139b92654f25bc3409496d3469
Ben Higginbotham <bicycleben88@gmail.com>
2021-09-24 01:20:12 -0400

    style delete page

--- components/DeleteBikeRide.tsx
+++ components/DeleteBikeRide.tsx
@@ -1,7 +1,10 @@
 import {useMutation, gql} from '@apollo/client';
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, StyleSheet} from 'react-native';
 import {GET_BIKE_RIDES} from './BikeRides';
+import {globalStyles} from '../styles/globalStyles';
+
+const {beige, h4, green, white} = globalStyles;
 
 const DELETE_BIKE_RIDE = gql`
   mutation DELETE_BIKE_RIDE($id: ID!) {
@@ -21,20 +24,41 @@ export default function DeleteBikeRide({route, navigation}) {
   });
 
   return (
-    <View>
+    <View style={styles.container}>
       {loading && <Text>Deleting bike ride</Text>}
       {error && <Text>{error.message} </Text>}
-      <Text>
+      <Text style={styles.header}>
         Are you sure you want to delete this bike ride from {item.date}?
       </Text>
-      <Button
-        onPress={() => {
-          deleteBikeRide();
-          navigation.navigate('Dashboard');
-        }}
-        title="Yes"
-      />
-      <Button onPress={() => navigation.navigate('Dashboard')} title="No" />
+      <View style={styles.button}>
+        <Button
+          onPress={() => {
+            deleteBikeRide();
+            navigation.navigate('Dashboard');
+          }}
+          title="Yes"
+        />
+        <Button onPress={() => navigation.navigate('Dashboard')} title="No" />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: beige,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  header: {
+    fontSize: h4,
+    backgroundColor: green,
+    color: white,
+    textAlign: 'center',
+  },
+});
